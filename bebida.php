bf490642f6d5f78ac1e1fd6dcf50c29654e907dd
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-11-15 12:21:52 +0100

    añadidos archivos

---
+++ bebida.php
@@ -0,0 +1,18 @@
+
+<?php 
+class Bebida extends Articulo {
+    private $alcoholica;
+    public function __construct($nombre,$coste,$precio,$contador,$alcoholica) {
+        parent::__construct($nombre,$coste,$precio,$contador);
+        $this->alcoholica=$alcoholica;
+    }
+    public function getAlcoholica() {
+        return $this->$alcoholica;
+    }
+    public function setAlcoholica($alcoholica){
+        $this->alcoholica=$alcoholica;
+    }
+      
+
+}
+?>
---
+++ vista.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nuestro Menú</h1>
+    <h2>Pizzas</h2>
+    <?php
+    echo $articulos[0];
+    ?>
+    <h2>Bebidas</h2>
+    <h2>Otros</h2>
+</body>
+</html>
